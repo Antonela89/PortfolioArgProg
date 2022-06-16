dd1d406b7f846bfc31a5e1c3551cd688ff9d702f
Antonela Borgogno <antonelaborgogno@gmail.com>
2022-06-16 17:48:45 -0300

    "header y banner, anexo boostrap

--- PorfolioArgProg/src/app/app.module.ts
+++ PorfolioArgProg/src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { LogoComponent } from './componentes/logo/logo.component';
-import { RedesSocialesComponent } from './redes/redes-sociales/redes-sociales.component';
+import { RedesSocialesComponent } from './componentes/redes-sociales/redes-sociales.component';
+import { BannerComponent } from './componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoComponent,
-    RedesSocialesComponent
+    RedesSocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
